cbf127944e9e57b2fb2383d2e9b5ee92b1a79a63
Rvfg <i@rvf6.com>
2026-08-16 03:42:56 +0800

    nixos/or1: enable shadowsocks

--- nixos/or1/configuration.nix
+++ nixos/or1/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   imports = [
     ./ehforwarderbot
@@ -14,6 +14,7 @@
   sops.defaultSopsFile = ./secrets.yaml;
   sops.secrets = {
     "wireguard_key".owner = "systemd-network";
+    "shadowsocks" = { };
   };
 
   networking.hostName = "or1";
@@ -21,6 +22,13 @@
   presets.wireguard.wg0 = {
     enable = true;
     mtu = 1400;
+  };
+
+  presets.shadowsocks = {
+    enable = true;
+    settings.server_port = 13926;
+    passwordFile = config.sops.secrets.shadowsocks.path;
+    openFirewall = true;
   };
 
   home-manager.users.rvfg = import ./home.nix;
